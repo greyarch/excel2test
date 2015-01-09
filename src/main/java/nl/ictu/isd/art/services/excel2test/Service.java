243bf4bac35ba79101de4ddfec69b2d4642eeb40
gantcho@greyarch.net <zlat1na>
2015-01-09 10:16:30 +0100

    init the upload repo with 10MB and a temp dir

--- src/main/java/nl/ictu/isd/art/services/excel2test/Service.java
+++ src/main/java/nl/ictu/isd/art/services/excel2test/Service.java
@@ -1,5 +1,6 @@
 package nl.ictu.isd.art.services.excel2test;
 
+import com.google.common.io.Files;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -24,14 +25,16 @@ import static spark.Spark.*;
 public class Service {
 
     private static final Logger LOG = Logger.getLogger("Excel to JSON service");
-    private final static TestScriptFactory FACTORY = new TestScriptFactory("");
+    private final static TestScriptFactory TEST_SCRIPT_FACTORY = new TestScriptFactory("");
+    private final static FileItemFactory FILE_FACTORY = new DiskFileItemFactory(10485760, Files.createTempDir()); //10MB thereshold
+    private final static ServletFileUpload UPLOAD = new ServletFileUpload(FILE_FACTORY);
 
     public static void main(String[] args) {
         get("/sheet/:sheet/file/*", (Request request, Response response) -> {
             try {
                 String fileName = "/" + request.splat()[0];
                 LOG.log(Level.INFO, "File to open {0}", fileName);
-                return FACTORY.createFromExcelSheet(new FileInputStream(fileName), request.params(":sheet")).toString();
+                return TEST_SCRIPT_FACTORY.createFromExcelSheet(new FileInputStream(fileName), request.params(":sheet")).toString();
             } catch (IOException | InvalidFormatException ex) {
                 Logger.getLogger(Service.class.getName()).log(Level.SEVERE, null, ex);
                 return "Error! " + ex.toString();
@@ -40,12 +43,10 @@ public class Service {
 
         post("/:sheet", (Request request, Response response) -> {
             List<FileItem> items = null;
-            FileItemFactory factory = new DiskFileItemFactory();
-            ServletFileUpload fu = new ServletFileUpload(factory);
             try {
-                items = fu.parseRequest(request.raw());
+                items = UPLOAD.parseRequest(request.raw());
                 InputStream content = items.get(0).getInputStream();
-                return FACTORY.createFromExcelSheet(content, request.params(":sheet")).toString();
+                return TEST_SCRIPT_FACTORY.createFromExcelSheet(content, request.params(":sheet")).toString();
             } catch (IOException | InvalidFormatException | FileUploadException ex) {
                 Logger.getLogger(Service.class.getName()).log(Level.SEVERE, "Unable to parse file", ex);
             }
